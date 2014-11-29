05b98112fc3ad7e779920211d9d69494d75e46ce
medtn <med.tn.sfax@gmail.com>
2014-11-29 19:41:01 +0100

    Signed-off-by: medtn <med.tn.sfax@gmail.com>

--- MyNotes/src/tn/devfest/mynotes/MainActivity.java
+++ MyNotes/src/tn/devfest/mynotes/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends Activity {
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        toogle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close);
+        //toogle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close);
         
         
         
