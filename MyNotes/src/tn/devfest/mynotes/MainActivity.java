ff27836c362c4220fa12e0425eff5911f905afa0
medtn <med.tn.sfax@gmail.com>
2014-11-29 20:17:37 +0100

    Signed-off-by: medtn <med.tn.sfax@gmail.com>

--- MyNotes/src/tn/devfest/mynotes/MainActivity.java
+++ MyNotes/src/tn/devfest/mynotes/MainActivity.java
@@ -3,14 +3,14 @@ package tn.devfest.mynotes;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
 	private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
@@ -25,8 +25,14 @@ public class MainActivity extends Activity {
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        //toogle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close);
+        toogle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_drawer_close);
         
+        mDrawerLayout.setDrawerListener(toogle);
+        
+        
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
         
         
     }
@@ -39,6 +45,13 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    @Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onPostCreate(savedInstanceState);
+		toogle.syncState();
+	}
+    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
